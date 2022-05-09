1c90c53cf593d07707fc7c61eac21f272df4edb2
Percy <pticllahuanacoh@unsa.edu.pe>
2022-05-09 11:09:37 -0500

    ejercicio01 js agregado

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var button_get_day = document.getElementById("getDay");
 button_get_day.onclick = function (){
     var value = document.getElementById("day").value;	
-    var days = ['domingo','lunes','martes','miercoles','jueves','viernes','sabado'];	// Arreglo con los días de la semana
+    var days = ['domingo','lunes','martes','miercoles','jueves','viernes','sabado'];
     var today = new Date();
     var result = document.getElementById("result_day");
     result.textContent =days[value];
