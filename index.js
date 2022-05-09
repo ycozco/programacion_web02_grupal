6233c3c89ef297bd9d453cbc666f9ac16a6bb436
yosetc <ycozco@unsa.edu.pe>
2022-05-08 21:39:45 -0500

    funcion add Table creada/ genera tabla a partir de los datos pasados, funcion table_sum

--- index.js
+++ index.js
@@ -6,3 +6,42 @@ button_meet.onclick = function (){
     document.getElementById("resultMeet").textContent = result;
     console.log(value);
 }
+function addTable() {
+    // get table div
+    var myTableDiv = document.getElementById("table_gen");
+    var column = document.getElementById("column").value;
+    var row = document.getElementById("row").value;
+    var table = document.createElement('TABLE');
+    table.border='1';
+    
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+      
+    for (var i=0; i<column; i++){
+       var tr = document.createElement('TR');
+       tableBody.appendChild(tr);
+       
+       for (var j=0; j<row; j++){
+           var td = document.createElement('TD');
+           td.width=row*50;
+           td.appendChild(document.createTextNode(""));
+           td.contentEditable =true;
+           tr.appendChild(td);
+       }
+    }
+    myTableDiv.append(table);
+}
+function table_sum(){
+    var column = document.getElementById("column").value;
+    var row = document.getElementById("row").value;
+    var sum = 0;
+    for (var i=0; i<column; i++){
+         var tr = document.getElementsByTagName('TR')[i];
+            for (var j=0; j<row; j++){
+                var td = tr.getElementsByTagName('TD')[j];
+                sum += parseInt(td.innerHTML);
+            }
+        }
+    document.getElementById("result_table").textContent = "La suma de la tabla es"+sum;
+}
+ 
