56f63b425a5a2e07f390ac41597a89c55707973d
César Alejandro Garay Bedregal <cgarayb@unsa.edu.pe>
2022-05-08 21:47:05 -0500

    Agregué la generación de las fechas, y las muestro con el console log para visualizar.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var button_missing_days = document.getElementById("missingDays");
 button_missing_days.onclick = function (){
-    console.log("Prueba")
+    var arequipa_day = new Date(2022,08,15);
+    var today = new Date();
+    var dif1 = arequipa_day-today;
+
+    console.log(arequipa_day);
+    console.log(today);
+    console.log(dif1);
 };
