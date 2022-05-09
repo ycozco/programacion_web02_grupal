3c5135ccefc5c2ee3bf60e75dd7192cfefea2ff4
César Alejandro Garay Bedregal <cgarayb@unsa.edu.pe>
2022-05-08 21:49:57 -0500

    Se agregó una linea donde se obtiene el dato en días de la diferencia de ambas fechas, además se agregó la linea que reemplaza el resultado al respectivo p

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ button_missing_days.onclick = function (){
     var arequipa_day = new Date(2022,08,15);
     var today = new Date();
     var dif1 = arequipa_day-today;
-
-    console.log(arequipa_day);
-    console.log(today);
-    console.log(dif1);
+    var result = parseInt(dif1/ (1000 * 3600 * 24));
+    var result_paragraf = document.getElementById("result_arequipa_day");
+    result_paragraf.textContent = "Faltan "+result + " dias.";
 };
