66e83073df94921dc5693a99ebdc68ee94ffae72
Vladimir Arturo Sulla Quispe <vsulaq@unsa.edu.pe>
2022-05-16 23:03:11 -0500

    ejercicio 2

--- lab03/pub/ejer02.js
+++ lab03/pub/ejer02.js
@@ -8,11 +8,41 @@ function listarMarkdown() {
             var length = data.text.length;
             text = '<ul>';
             for (var i = 0; i < length; i++) {
-                text += '<li><a href="http://localhost:3000/' + data.text[i] + '">' + data.text[i] + '</a></li>';
+                text += '<li><a href="http://localhost:3000/' + data.text[i] + '">' + data.text[i] + '</a></li><button onclick="' + 'recitar("' + data.text[i] + '")">mostrar</button>';
             }
             text += '</ul>';
-            
+
             document.querySelector("#lista").innerHTML = text;
         }
     )
 }
+function recitar(markupText) {
+    const url = 'http://localhost:3000/'
+    const data = {
+        text: markupText
+    }
+    console.log(data)
+    const request = {
+        method: 'POST', // Podría ser GET
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    }
+    http = fetch(url, request)
+    http.then(
+        response => response.json()
+    ).then(
+        data => {
+            document.querySelector("#htmlCode").innerHTML = data.text
+        }
+    )
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const text = document.querySelector('#markupText')
+    document.querySelector('#markupForm').onsubmit = () => {
+        recitar(text.value)
+        return false;
+    }
+})
